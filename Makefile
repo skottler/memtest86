c9c17f84ad4cc8a807baa1c31f3da6d81e6d5113
Sam Kottler <shk@linux.com>
2015-08-10 09:54:39 -0400

    switch back to 4.20

--- Makefile
+++ Makefile
@@ -3,7 +3,6 @@
 # Author:		Chris Brady
 # Created:		January 1, 1996
 
-
 #
 # Path for the floppy disk device
 #
@@ -12,22 +11,20 @@ FDISK=/dev/fd0
 AS=as -32
 CC=gcc
 
-CFLAGS= -Wall -march=i486 -m32 -O1 -fomit-frame-pointer -fno-builtin \
-	-ffreestanding -fPIC $(SMP_FL) -fno-stack-protector 
-	
-OBJS= head.o reloc.o main.o test.o init.o lib.o patn.o screen_buffer.o \
-      config.o cpuid.o linuxbios.o pci.o memsize.o spd.o error.o dmi.o controller.o \
-      smp.o vmem.o random.o
-      
+CFLAGS= -Wall -march=i486 -m32 -O2 -fomit-frame-pointer -fno-builtin -ffreestanding -fPIC -fno-stack-protector
 
-all: clean memtest.bin memtest 
+OBJS= head.o reloc.o main.o test.o init.o lib.o patn.o screen_buffer.o \
+      config.o linuxbios.o memsize.o pci.o controller.o random.o spd.o \
+      error.o dmi.o cpuid.o
+
+all: memtest.bin memtest
 
 # Link it statically once so I know I don't have undefined
 # symbols and then link it dynamically so I have full
 # relocation information
 memtest_shared: $(OBJS) memtest_shared.lds Makefile
 	$(LD) --warn-constructors --warn-common -static -T memtest_shared.lds \
-	 -o $@ $(OBJS) && \
+	-o $@ $(OBJS) && \
 	$(LD) -shared -Bsymbolic -T memtest_shared.lds -o $@ $(OBJS)
 
 memtest_shared.bin: memtest_shared
@@ -53,22 +50,18 @@ reloc.o: reloc.c
 	$(CC) -c $(CFLAGS) -fno-strict-aliasing reloc.c
 
 test.o: test.c
-	$(CC) -c -Wall -march=i486 -m32 -O0 -fomit-frame-pointer -fno-builtin -ffreestanding test.c
-
-random.o: random.c
-	$(CC) -c -Wall -march=i486 -m32 -O3 -fomit-frame-pointer -fno-builtin -ffreestanding random.c
-	
-# rule for build number generation  
-build_number:
-	sh make_buildnum.sh  
+	$(CC) -c -Wall -march=i486 -m32 -Os -fomit-frame-pointer -fno-builtin -ffreestanding test.c
 
 clean:
-	rm -f *.o *.s *.iso memtest.bin memtest memtest_shared \
-		memtest_shared.bin memtest.iso
+	rm -f *.o *.s *.iso memtest.bin memtest memtest_shared memtest_shared.bin
+
+asm:
+	@./makedos.sh
 
 iso:
 	make all
 	./makeiso.sh
+	rm -f *.o *.s memtest.bin memtest memtest_shared memtest_shared.bin
 
 install: all
 	dd <memtest.bin >$(FDISK) bs=8192
@@ -78,3 +71,4 @@ install-precomp:
 	
 dos: all
 	cat mt86+_loader memtest.bin > memtest.exe
+
--- spd.c
+++ spd.c
@@ -1,5 +1,9 @@
-/*
- * MemTest86+ V5 Specific code (GPL V2.0)
+/* Memtest86 SPD extension 
+ * added by Reto Sonderegger, 2004, reto@swissbit.com
+ * 
+ * Released under version 2 of the Gnu Puclic License
+ * ----------------------------------------------------
+ * MemTest86+ V4.20 Specific code (GPL V2.0)
  * By Samuel DEMEULEMEESTER, sdemeule@memtest.org
  * http://www.canardpc.com - http://www.memtest.org
  */
@@ -25,7 +29,7 @@ extern void wait_keyup();
 
 int smbdev, smbfun;
 unsigned short smbusbase;
-unsigned char spd_raw[256];
+unsigned char spd[256];
 char s[] = {'/', 0, '-', 0, '\\', 0, '|', 0};	
 
 static void ich5_get_smb(void)
@@ -34,40 +38,6 @@ static void ich5_get_smb(void)
     int result;
     result = pci_conf_read(0, smbdev, smbfun, 0x20, 2, &x);
     if (result == 0) smbusbase = (unsigned short) x & 0xFFFE;
-}
-
-static void piix4_get_smb(void)
-{
-    unsigned long x;
-    int result;
-    
-    result = pci_conf_read(0, smbdev, smbfun, 0x08, 1, &x);
-    
-    if(x < 0x40){   
- 			// SB600/700
- 			result = pci_conf_read(0, smbdev, smbfun, 0x90, 2, &x);
-  		if (result == 0) smbusbase = (unsigned short) x & 0xFFFE;
-  	} else {
-  		// SB800
-			sb800_get_smb();
-  	} 	
-}
-
-void sb800_get_smb(void)
-{
-	  int lbyte, hbyte;
-	
-		__outb(AMD_SMBUS_BASE_REG + 1, AMD_INDEX_IO_PORT);	
-		lbyte = __inb(AMD_DATA_IO_PORT);
-		__outb(AMD_SMBUS_BASE_REG, AMD_INDEX_IO_PORT);	
-		hbyte = __inb(AMD_DATA_IO_PORT);
-		
-		smbusbase = lbyte;
-		smbusbase <<= 8;
-		smbusbase += hbyte;
-		smbusbase &= 0xFFE0;
-		
-		if (smbusbase == 0xFFE0)	{ smbusbase = 0; }	
 }
 
 unsigned char ich5_smb_read_byte(unsigned char adr, unsigned char cmd)
@@ -83,9 +53,9 @@ unsigned char ich5_smb_read_byte(unsigned char adr, unsigned char cmd)
     rdtsc(l1, h1);
     //cprint(POP2_Y, POP2_X + 16, s + cmd % 8);	// progress bar
     while (!(__inb(SMBHSTSTS) & 0x02)) {	// wait til command finished
-			rdtsc(l2, h2);
-			t = ((h2 - h1) * 0xffffffff + (l2 - l1)) / v->clks_msec;
-			if (t > 10) break;			// break after 10ms
+	rdtsc(l2, h2);
+	t = ((h2 - h1) * 0xffffffff + (l2 - l1)) / v->clks_msec;
+	if (t > 10) break;			// break after 10ms
     }
     return __inb(SMBHSTDAT);
 }
@@ -93,10 +63,10 @@ unsigned char ich5_smb_read_byte(unsigned char adr, unsigned char cmd)
 static int ich5_read_spd(int dimmadr)
 {
     int x;
-    spd_raw[0] = ich5_smb_read_byte(0x50 + dimmadr, 0);
-    if (spd_raw[0] == 0xff)	return -1;		// no spd here
+    spd[0] = ich5_smb_read_byte(0x50 + dimmadr, 0);
+    if (spd[0] == 0xff)	return -1;		// no spd here
     for (x = 1; x < 256; x++) {
-			spd_raw[x] = ich5_smb_read_byte(0x50 + dimmadr, (unsigned char) x);
+			spd[x] = ich5_smb_read_byte(0x50 + dimmadr, (unsigned char) x);
     }
     return 0;
 }
@@ -136,10 +106,10 @@ unsigned char us15w_smb_read_byte(unsigned char adr, unsigned char cmd)
 static int us15w_read_spd(int dimmadr)
 {
     int x;
-    spd_raw[0] = us15w_smb_read_byte(0x50 + dimmadr, 0);
-    if (spd_raw[0] == 0xff)	return -1;		// no spd here
+    spd[0] = us15w_smb_read_byte(0x50 + dimmadr, 0);
+    if (spd[0] == 0xff)	return -1;		// no spd here
     for (x = 1; x < 256; x++) {
-	spd_raw[x] = us15w_smb_read_byte(0x50 + dimmadr, (unsigned char) x);
+	spd[x] = us15w_smb_read_byte(0x50 + dimmadr, (unsigned char) x);
     }
     return 0;
 }
@@ -153,29 +123,21 @@ struct pci_smbus_controller {
 };
 
 static struct pci_smbus_controller smbcontrollers[] = {
-	// Intel SMBUS
-	{0x8086, 0x9C22, "Intel HSW-ULT",	ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x8C22, "Intel HSW", 		ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x1E22, "Intel Z77", 		ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x1C22, "Intel P67", 		ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x3B30, "Intel P55", 		ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x3A60, "Intel ICH10B", 	ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x3A30, "Intel ICH10R", 	ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x2930, "Intel ICH9", 		ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x283E, "Intel ICH8", 		ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x27DA, "Intel ICH7", 		ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x266A, "Intel ICH6", 		ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x24D3, "Intel ICH5", 		ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x24C3, "Intel ICH4", 		ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x25A4, "Intel 6300ESB", ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x269B, "Intel ESB2", 		ich5_get_smb, ich5_read_spd},
-	{0x8086, 0x8119, "Intel US15W", 	us15w_get_smb, us15w_read_spd},
-	{0x8086, 0x5032, "Intel EP80579", ich5_get_smb, ich5_read_spd},
-	
-	// AMD SMBUS
-	{0x1002, 0x4385, "AMD SB600/700",	piix4_get_smb, ich5_read_spd},
-	{0x1022, 0x780B, "AMD SB800/900", sb800_get_smb, ich5_read_spd},
-	{0, 0, "", NULL, NULL}
+{0x8086, 0x1C22, "Intel P67", 		ich5_get_smb, ich5_read_spd},
+{0x8086, 0x3B30, "Intel P55", 		ich5_get_smb, ich5_read_spd},
+{0x8086, 0x3A60, "Intel ICH10B", 	ich5_get_smb, ich5_read_spd},
+{0x8086, 0x3A30, "Intel ICH10R", 	ich5_get_smb, ich5_read_spd},
+{0x8086, 0x2930, "Intel ICH9", 		ich5_get_smb, ich5_read_spd},
+{0x8086, 0x283E, "Intel ICH8", 		ich5_get_smb, ich5_read_spd},
+{0x8086, 0x27DA, "Intel ICH7", 		ich5_get_smb, ich5_read_spd},
+{0x8086, 0x266A, "Intel ICH6", 		ich5_get_smb, ich5_read_spd},
+{0x8086, 0x24D3, "Intel ICH5", 		ich5_get_smb, ich5_read_spd},
+{0x8086, 0x24C3, "Intel ICH4", 		ich5_get_smb, ich5_read_spd},
+{0x8086, 0x25A4, "Intel 6300ESB", ich5_get_smb, ich5_read_spd},
+{0x8086, 0x269B, "Intel ESB2", 		ich5_get_smb, ich5_read_spd},
+{0x8086, 0x8119, "Intel US15W", 	us15w_get_smb, us15w_read_spd},
+{0x8086, 0x5032, "Intel EP80579", ich5_get_smb, ich5_read_spd},
+{0, 0, "", NULL, NULL}
 };
 
 
@@ -183,7 +145,6 @@ int find_smb_controller(void)
 {
     int i = 0;
     unsigned long valuev, valued;
-    
     for (smbdev = 0; smbdev < 32; smbdev++) {
 			for (smbfun = 0; smbfun < 8; smbfun++) {
 		    pci_conf_read(0, smbdev, smbfun, 0, 2, &valuev);
@@ -212,7 +173,6 @@ void get_spd_spec(void)
     int module_size;
     int curcol;
     int temp_nbd;
-    int tck;
 
     index = find_smb_controller();
     
@@ -224,75 +184,59 @@ void get_spd_spec(void)
 
     smbcontrollers[index].get_adr();
 		cprint(LINE_SPD-2, 0, "Memory SPD Informations");
-		cprint(LINE_SPD-1, 0, "--------------------------");    
+		cprint(LINE_SPD-1, 0, "-----------------------------------");    
 		
     for (j = 0; j < 8; j++) {
 			if (smbcontrollers[index].read_spd(j) == 0) {	
 				curcol = 1;
-				if(spd_raw[2] == 0x0b){
+				if(spd[2] == 0x0b){
 				  // We are here if DDR3 present
 				 
 				  // First print slot#, module capacity
 					cprint(LINE_SPD+k, curcol, " - Slot   :");
 					dprint(LINE_SPD+k, curcol+8, k, 1, 0);
 
-					module_size = get_ddr3_module_size(spd_raw[4] & 0xF, spd_raw[8] & 0x7, spd_raw[7] & 0x7, spd_raw[7] >> 3);
+					module_size = get_ddr3_module_size(spd[4] & 0xF, spd[8] & 0x7, spd[7] & 0x7, spd[7] >> 3);
 					temp_nbd = getnum(module_size); curcol += 12;
 					dprint(LINE_SPD+k, curcol, module_size, temp_nbd, 0); curcol += temp_nbd;
 					cprint(LINE_SPD+k, curcol, " MB"); curcol += 4;
 					
-					// If XMP is supported, check Tck in XMP reg					
-					if(spd_raw[176] == 0x0C && spd_raw[177] == 0x4A && spd_raw[12])
-						{
-							tck = spd_raw[186];
-						} else {
-							tck = spd_raw[12];
-						}
-					
 					// Then module jedec speed
-					switch(tck)
+					switch(spd[12])
 					{
 						default:
-							cprint(LINE_SPD+k, curcol, "DDR3-????");
-							break;						
 						case 20:
-							cprint(LINE_SPD+k, curcol, "DDR3-800");
-							curcol--;
+							cprint(LINE_SPD+k, curcol, "PC3-6400");
+							curcol += 8;
 							break;
 						case 15:
-							cprint(LINE_SPD+k, curcol, "DDR3-1066");
+							cprint(LINE_SPD+k, curcol, "PC3-8500");
+							curcol += 8;
 							break;
 						case 12:
-							cprint(LINE_SPD+k, curcol, "DDR3-1333");
+							cprint(LINE_SPD+k, curcol, "PC3-10600");
+							curcol += 9;
 							break;
 						case 10:
-							cprint(LINE_SPD+k, curcol, "DDR3-1600");
-							break;
-						case 9:
-							cprint(LINE_SPD+k, curcol, "DDR3-1866");
+							cprint(LINE_SPD+k, curcol, "PC3-12800");
+							curcol += 9;
 							break;
 						case 8:
-							cprint(LINE_SPD+k, curcol, "DDR3-2133");
-							break;
-						case 7:
-							cprint(LINE_SPD+k, curcol, "DDR3-2400");
+							cprint(LINE_SPD+k, curcol, "PC3-15000");
+							curcol += 9;
 							break;
 						case 6:
-							cprint(LINE_SPD+k, curcol, "DDR3-2533");
-							break;
-						case 5:
-							cprint(LINE_SPD+k, curcol, "DDR3-2666");
+							cprint(LINE_SPD+k, curcol, "PC3-16000");
+							curcol += 9;
 							break;
 						}
 					
-					curcol += 10;
-					
-					if((spd_raw[8] >> 3) == 1) { cprint(LINE_SPD+k, curcol, "ECC"); curcol += 4; }
+					curcol++;
 					
 					// Then print module infos (manufacturer & part number)	
-					spd_raw[117] &= 0x0F; // Parity odd or even
+					spd[117] &= 0x0F; // Parity odd or even
 					for (i = 0; jep106[i].cont_code < 9; i++) {	
-			    	if (spd_raw[117] == jep106[i].cont_code && spd_raw[118] == jep106[i].hex_byte) {
+			    	if (spd[117] == jep106[i].cont_code && spd[118] == jep106[i].hex_byte) {
 			    		// We are here if a Jedec manufacturer is detected
 							cprint(LINE_SPD+k, curcol, "-"); curcol += 2;							
 							cprint(LINE_SPD+k, curcol, jep106[i].name);
@@ -300,25 +244,12 @@ void get_spd_spec(void)
 							curcol++;
 							// Display module serial number
 							for (h = 128; h < 146; h++) {	
-								cprint(LINE_SPD+k, curcol, convert_hex_to_char(spd_raw[h]));
+								cprint(16+k, curcol, convert_hex_to_char(spd[h]));
 								curcol++;		
 							}			
-
-							// Detect Week and Year of Manufacturing (Think to upgrade after 2015 !!!)
-							if(curcol <= 72 && spd_raw[120] > 3 && spd_raw[120] < 16 && spd_raw[121] < 55)
-							{
-								cprint(LINE_SPD+k, curcol, "(W");	
-								dprint(LINE_SPD+k, curcol+2, spd_raw[121], 2, 0);
-								if(spd_raw[121] < 10) { cprint(LINE_SPD+k, curcol+2, "0"); }
-								cprint(LINE_SPD+k, curcol+4, "'");	
-								dprint(LINE_SPD+k, curcol+5, spd_raw[120], 2, 0);
-								if(spd_raw[120] < 10) { cprint(LINE_SPD+k, curcol+5, "0"); }
-								cprint(LINE_SPD+k, curcol+7, ")");	
-								curcol += 9;
-							}															
 															
 							// Detect XMP Memory
-							if(spd_raw[176] == 0x0C && spd_raw[177] == 0x4A)
+							if(spd[176] == 0x0C && spd[177] == 0x4A)
 								{
 									cprint(LINE_SPD+k, curcol, "*XMP*");					
 								}
@@ -326,12 +257,12 @@ void get_spd_spec(void)
 					}		
 				}
 			// We enter this function if DDR2 is detected
-			if(spd_raw[2] == 0x08){				
+			if(spd[2] == 0x08){				
 					 // First print slot#, module capacity
 					cprint(LINE_SPD+k, curcol, " - Slot   :");
 					dprint(LINE_SPD+k, curcol+8, k, 1, 0);
 
-					module_size = get_ddr2_module_size(spd_raw[31], spd_raw[5]);
+					module_size = get_ddr2_module_size(spd[31], spd[5]);
 					temp_nbd = getnum(module_size); curcol += 12;
 					dprint(LINE_SPD+k, curcol, module_size, temp_nbd, 0); curcol += temp_nbd;
 					cprint(LINE_SPD+k, curcol, " MB"); curcol += 4;		
@@ -339,29 +270,27 @@ void get_spd_spec(void)
 					// Then module jedec speed
 					float ddr2_speed, byte1, byte2;
 					
-					byte1 = (spd_raw[9] >> 4) * 10;
-					byte2 = spd_raw[9] & 0xF;
+					byte1 = (spd[9] >> 4) * 10;
+					byte2 = spd[9] & 0xF;
 					
-					ddr2_speed = 1 / (byte1 + byte2) * 10000 * 2;
+					ddr2_speed = 1 / (byte1 + byte2) * 10000;
 
 					temp_nbd = getnum(ddr2_speed);
 					cprint(LINE_SPD+k, curcol, "DDR2-"); curcol += 5;	 
 					dprint(LINE_SPD+k, curcol, ddr2_speed, temp_nbd, 0); curcol += temp_nbd;
-
-					if((spd_raw[11] >> 1) == 1) { cprint(LINE_SPD+k, curcol+1, "ECC"); curcol += 4; }
 			
 					// Then print module infos (manufacturer & part number)	
 					int ccode = 0;
 					
 					for(i = 64; i < 72; i++)
 					{
-						if(spd_raw[i] == 0x7F) { ccode++; }			
+						if(spd[i] == 0x7F) { ccode++; }			
 					}
 					
 					curcol++;
 					
 					for (i = 0; jep106[i].cont_code < 9; i++) {	
-			    	if (ccode == jep106[i].cont_code && spd_raw[64+ccode] == jep106[i].hex_byte) {
+			    	if (ccode == jep106[i].cont_code && spd[64+ccode] == jep106[i].hex_byte) {
 			    		// We are here if a Jedec manufacturer is detected
 							cprint(LINE_SPD+k, curcol, "-"); curcol += 2;							
 							cprint(LINE_SPD+k, curcol, jep106[i].name);
@@ -369,7 +298,7 @@ void get_spd_spec(void)
 							curcol++;
 							// Display module serial number
 							for (h = 73; h < 91; h++) {	
-								cprint(LINE_SPD+k, curcol, convert_hex_to_char(spd_raw[h]));
+								cprint(16+k, curcol, convert_hex_to_char(spd[h]));
 								curcol++;		
 							}			
 															
@@ -404,7 +333,7 @@ void show_spd(void)
 	if (smbcontrollers[index].read_spd(j) == 0) {
 	    dprint(POP2_Y, POP2_X + 15, j, 2, 0);		
     	    for (i = 0; i < 256; i++) {
-		hprint2(2 + POP2_Y + i / 16, 3 + POP2_X + (i % 16) * 3, spd_raw[i], 2);
+		hprint2(2 + POP2_Y + i / 16, 3 + POP2_X + (i % 16) * 3, spd[i], 2);
 	    }
 	    flag = 0;
     	    while(!flag) {
